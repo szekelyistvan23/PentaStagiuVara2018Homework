eb7aa374dc4d31f4ec1f210e2e4b6b41bbaa9ed3
Szekely Istvan <iistvann@yahoo.com>
2018-07-09 13:14:45 +0300

    Homework Module03Week01

---
+++ Module03Week01/CommonBoard/Controllers/PostsController.cs
@@ -0,0 +1,51 @@
+﻿using CommonBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommonBoard.Controllers
+{
+    public class PostsController : Controller
+    {
+        public static List<Post> posts = new List<Post>();
+        public static int Id = 0;
+
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return View(posts);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                Id++;
+                post.Id = Id;
+                posts.Add(post);
+                return RedirectToAction("Details");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
+            int lastPost = posts.Count;
+            Post post = posts.Find(p => p.Id == lastPost);
+            return View(post);
+        }
+    }
+}
---
+++ Module03Week01/CommonBoard/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommonBoard.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime TimeOfPosting { get; set; }
+        public string Message { get; set; }
+        public MessageType PostType { get; set; }
+        public bool isSticky { get; set; }
+        public int Priority { get; set; }
+    }
+    public enum MessageType
+    {
+        Text,
+        Photos
+    }
+}
